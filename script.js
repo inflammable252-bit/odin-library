6c926fa51844750150cb84cd07753e0302626dcf
MT <221427704+inflammable252-bit@users.noreply.github.com>
2026-08-16 18:07:00 -0700

    Add form validation with custom errors

--- script.js
+++ script.js
@@ -1,252 +1,309 @@
-const myLibrary=[];
+const myLibrary = [];
 const cardSection = document.querySelector("div#card-wrapper");
 
 class Book {
-    constructor(title, author, pages, read, note) {
-        this.id = crypto.randomUUID();
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-        this.note = note;
-        this.hslAngle= Math.floor(Math.random()*361);
-        this.info = `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}. Note: "${note}". ID: ${this.id}`;
+	constructor(title, author, pages, read, note) {
+		this.id = crypto.randomUUID();
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = read;
+		this.note = note;
+		this.hslAngle = Math.floor(Math.random() * 361);
+		this.info = `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}. Note: "${note}". ID: ${this.id}`;
 
-        this.updateReadInLibrary = function() {
-            this.read === "read" ? this.read ="not read" : this.read = "read";
-            console.log(`${this.title} read status changed to "${this.read}"`)
-        }
-    }
+		this.updateReadInLibrary = function () {
+			this.read === "read" ? (this.read = "not read") : (this.read = "read");
+			console.log(`${this.title} read status changed to "${this.read}"`);
+		};
+	}
 }
 function addBookToLibrary(title, author, pages, read, note) {
-    const newBook = new Book(title, author, pages, read, note);
-    myLibrary.unshift(newBook);
-    createShelves();
+	const newBook = new Book(title, author, pages, read, note);
+	myLibrary.unshift(newBook);
+	createShelves();
 }
 
 let cards;
 
 function removePreviousCards() {
-    cards = document.querySelectorAll("div.book-div");
-    cards.forEach((item)=> {item.remove()});
+	cards = document.querySelectorAll("div.book-div");
+	cards.forEach((item) => {
+		item.remove();
+	});
 }
 
 let shelves = [];
 let numberOfShelves;
 
 function createShelves() {
-    const myLibraryCopy = [...myLibrary];
-    numberOfShelves = (Math.ceil(myLibrary.length/4));
-    for (i=1; i<=numberOfShelves; i++) {
-        shelves[i] = myLibraryCopy.splice(0,4);
-    }
+	const myLibraryCopy = [...myLibrary];
+	numberOfShelves = Math.ceil(myLibrary.length / 4);
+	for (i = 1; i <= numberOfShelves; i++) {
+		shelves[i] = myLibraryCopy.splice(0, 4);
+	}
 }
 
 let cardPage = 1;
 
 function updateCards() {
-    if (cardPage>numberOfShelves) {
-        toPreviousPage();
-    }
-    removePreviousCards();
-    
-    let currentShelf = shelves[cardPage];
-    if (currentShelf===0) return;
-    currentShelf.forEach((item) => {
-    const book = document.createElement("article");
-    book.classList.add("card");
+	if (cardPage > numberOfShelves) {
+		toPreviousPage();
+	}
+	removePreviousCards();
 
-    bookTitle = document.createElement("p");
-    bookTitle.textContent = item.title;
-    book.appendChild(bookTitle);
-    bookAuthor=document.createElement("p");
-    bookAuthor.textContent=item.author;
-    book.appendChild(bookAuthor);
-    pageCount=document.createElement("p");
-    pageCount.textContent=`${item.pages} pages`;
-    book.appendChild(pageCount)
+	let currentShelf = shelves[cardPage];
+	if (currentShelf === 0) return;
+	currentShelf.forEach((item) => {
+		const book = document.createElement("article");
+		book.classList.add("card");
 
-    bookNote=document.createElement("p");
-    bookNote.classList.add("note");
-    if (item.note) {bookNote.textContent=`"${item.note}"`};
-    book.appendChild(bookNote);
+		bookTitle = document.createElement("p");
+		bookTitle.textContent = item.title;
+		book.appendChild(bookTitle);
+		bookAuthor = document.createElement("p");
+		bookAuthor.textContent = item.author;
+		book.appendChild(bookAuthor);
+		pageCount = document.createElement("p");
+		pageCount.textContent = `${item.pages} pages`;
+		book.appendChild(pageCount);
 
-    readStatus=document.createElement("p");
-    readStatus.textContent=item.read;
-        if (item.read==="read") {
-            readStatus.textContent="Read"
-            readStatus.className="read";
-        }
-        else {
-            readStatus.textContent="Not read"
-            readStatus.className="notRead"
-        }
-    book.appendChild(readStatus);
+		bookNote = document.createElement("p");
+		bookNote.classList.add("note");
+		if (item.note) {
+			bookNote.textContent = `"${item.note}"`;
+		}
+		book.appendChild(bookNote);
 
-    const editButtons = document.createElement("div");
-    editButtons.classList.add("edit");
-    const deleteButton = document.createElement("button");
-    deleteButton.classList.add("delete")
-    const xIcon = document.createElement("img");
-    xIcon.src = "./cross-svgrepo-com.png";
-    deleteButton.append(xIcon);
-    deleteButton.id = item.id;
-    const readButton = document.createElement("button");
-    readButton.classList.add("read");
-    readButton.id = item.id;
-    const readIcon = document.createElement("img");
-    readIcon.src = "./book-open-svgrepo-com.png";
-    readButton.append(readIcon);
+		readStatus = document.createElement("p");
+		readStatus.textContent = item.read;
+		if (item.read === "read") {
+			readStatus.textContent = "Read";
+			readStatus.className = "read";
+		} else {
+			readStatus.textContent = "Not read";
+			readStatus.className = "notRead";
+		}
+		book.appendChild(readStatus);
 
-    editButtons.appendChild(deleteButton);
-    editButtons.appendChild(readButton)
-    
-    book.style.backgroundColor=`hsl(${item.hslAngle}, 50%, 75%)`;
-    
-    const bookDiv = document.createElement("div");
-    bookDiv.classList.add("book-div");
-    bookDiv.id = item.id;
+		const editButtons = document.createElement("div");
+		editButtons.classList.add("edit");
+		const deleteButton = document.createElement("button");
+		deleteButton.classList.add("delete");
+		const xIcon = document.createElement("img");
+		xIcon.src = "./cross-svgrepo-com.png";
+		deleteButton.append(xIcon);
+		deleteButton.id = item.id;
+		const readButton = document.createElement("button");
+		readButton.classList.add("read");
+		readButton.id = item.id;
+		const readIcon = document.createElement("img");
+		readIcon.src = "./book-open-svgrepo-com.png";
+		readButton.append(readIcon);
 
-    bookDiv.appendChild(book);
-    bookDiv.appendChild(editButtons);
+		editButtons.appendChild(deleteButton);
+		editButtons.appendChild(readButton);
 
-    cardSection.appendChild(bookDiv);
-    })
-    const allDeleteButtons = document.querySelectorAll("button.delete");
-    allDeleteButtons.forEach((item) => {
-        item.addEventListener("click", () => {
-            deleteBook(item.id)
-            })
-        })
+		book.style.backgroundColor = `hsl(${item.hslAngle}, 50%, 75%)`;
 
-    const allReadButtons = document.querySelectorAll("button.read");
-    allReadButtons.forEach((item) => {
-        item.addEventListener("click", (e) => {
-        idToToggle = item.id;
-        function linkIDtoLibrary(id) {
-            myLibrary.forEach((book) => {
-                if (book.id===id) {
-                    return libraryItem = book
-                    } 
-                })
-            }
-        linkIDtoLibrary(idToToggle)
-        libraryItem.updateReadInLibrary();
-        const readStatusParentCard = e.currentTarget.parentElement.parentElement;
-        readTextInCard = readStatusParentCard.querySelector("p:last-child");
-        readTextInCard.className = readTextInCard.className === "read" ? "notRead" : "read";
-        readTextInCard.textContent = readTextInCard.textContent === "Read" ? "Not read" : "Read";
-        })
-    })
+		const bookDiv = document.createElement("div");
+		bookDiv.classList.add("book-div");
+		bookDiv.id = item.id;
 
-    checkButtons()
+		bookDiv.appendChild(book);
+		bookDiv.appendChild(editButtons);
 
-    console.log(myLibrary)
-    console.log(shelves)
-    console.log(`# of shelves: ${numberOfShelves} / total books: ${myLibrary.length}`)
-    console.log(`Card page: ${cardPage}`)
+		cardSection.appendChild(bookDiv);
+	});
+	const allDeleteButtons = document.querySelectorAll("button.delete");
+	allDeleteButtons.forEach((item) => {
+		item.addEventListener("click", () => {
+			deleteBook(item.id);
+		});
+	});
+
+	const allReadButtons = document.querySelectorAll("button.read");
+	allReadButtons.forEach((item) => {
+		item.addEventListener("click", (e) => {
+			idToToggle = item.id;
+			function linkIDtoLibrary(id) {
+				myLibrary.forEach((book) => {
+					if (book.id === id) {
+						return (libraryItem = book);
+					}
+				});
+			}
+			linkIDtoLibrary(idToToggle);
+			libraryItem.updateReadInLibrary();
+			const readStatusParentCard = e.currentTarget.parentElement.parentElement;
+			readTextInCard = readStatusParentCard.querySelector("p:last-child");
+			readTextInCard.className =
+				readTextInCard.className === "read" ? "notRead" : "read";
+			readTextInCard.textContent =
+				readTextInCard.textContent === "Read" ? "Not read" : "Read";
+		});
+	});
+
+	checkButtons();
+
+	console.log(myLibrary);
+	console.log(shelves);
+	console.log(
+		`# of shelves: ${numberOfShelves} / total books: ${myLibrary.length}`,
+	);
+	console.log(`Card page: ${cardPage}`);
 }
 
 function deleteBook(idToDelete) {
-    console.log(idToDelete)
-    myLibrary.forEach((item) => {
-        if (item.id === idToDelete) {
-            const itemIndex = myLibrary.indexOf(item);
-            myLibrary.splice(itemIndex, 1);
-            console.log(`Deleted ${item.title}: ${item.id}`)
-            shelves.length=0;
-            createShelves();
-            updateCards();
-            }
-        })
+	console.log(idToDelete);
+	myLibrary.forEach((item) => {
+		if (item.id === idToDelete) {
+			const itemIndex = myLibrary.indexOf(item);
+			myLibrary.splice(itemIndex, 1);
+			console.log(`Deleted ${item.title}: ${item.id}`);
+			shelves.length = 0;
+			createShelves();
+			updateCards();
+		}
+	});
 }
 
 const pageDisplay = document.getElementById("pageNumber");
 const previousButton = document.getElementById("previous");
 const nextButton = document.getElementById("next");
 const pageButtons = document.getElementById("page-wrapper");
-const noteToggle = document.getElementById("toggle-notes")
+const noteToggle = document.getElementById("toggle-notes");
 
 pageButtons.addEventListener("click", (e) => {
-    switch (e.target.id) {
-        case "previous":
-            toPreviousPage();
-            break
-        case "next":
-            toNextPage();
-            break;
-        case "toggle-notes":
-            toggleNotes();
-            break;
-    }
-})
-    function toPreviousPage() {
-        if (cardPage===1) return;
-        else {
-        cardPage--; 
-        updatePage();
-        updateCards();
-        }
-    }
-    function toNextPage() {
-        if (cardPage === numberOfShelves) return;
-        else {
-        cardPage++;
-        updatePage(); 
-        updateCards();
-        }
-    }
-    function updatePage() {
-        pageDisplay.textContent = cardPage;
-    }
-    function toggleNotes() {
-        cardSection.classList.toggle("toggle-notes");
-    }
+	switch (e.target.id) {
+		case "previous":
+			toPreviousPage();
+			break;
+		case "next":
+			toNextPage();
+			break;
+		case "toggle-notes":
+			toggleNotes();
+			break;
+	}
+});
+function toPreviousPage() {
+	if (cardPage === 1) return;
+	else {
+		cardPage--;
+		updatePage();
+		updateCards();
+	}
+}
+function toNextPage() {
+	if (cardPage === numberOfShelves) return;
+	else {
+		cardPage++;
+		updatePage();
+		updateCards();
+	}
+}
+function updatePage() {
+	pageDisplay.textContent = cardPage;
+}
+function toggleNotes() {
+	cardSection.classList.toggle("toggle-notes");
+}
 
 function checkButtons() {
-    if (cardPage===1) {
-        previousButton.style.opacity = 0.3;
-    }
-    else previousButton.style.opacity = 0.7;
-    if (cardPage===numberOfShelves) {
-        nextButton.style.opacity = 0.3;
-    }
-    else nextButton.style.opacity=0.7;
+	if (cardPage === 1) {
+		previousButton.style.opacity = 0.3;
+	} else previousButton.style.opacity = 0.7;
+	if (cardPage === numberOfShelves) {
+		nextButton.style.opacity = 0.3;
+	} else nextButton.style.opacity = 0.7;
 }
+
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const read = document.getElementById("read");
 
 const bookForm = document.getElementById("bookform");
 const createBookButton = document.querySelector("button#create");
 
-createBookButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    if (bookForm.title.value, bookForm.author.value, bookForm.pages.value) {
-    createBookFromForm()
-    }
+bookForm.addEventListener("input", () => {
+	validateInputs();
+});
+
+bookForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	validateInputs();
+	if (bookForm.checkValidity()) {
+		createBookFromForm();
+	} else {
+		bookForm.reportValidity();
+	}
+});
+
+function validateInputs() {
+	if (title.validity.valueMissing) {
+		title.setCustomValidity("Your book must have a name!");
+	} else title.setCustomValidity("");
+	if (author.validity.valueMissing) {
+		author.setCustomValidity("Your book must have an author!");
+	} else author.setCustomValidity("");
+	if (pages.validity.valueMissing) {
+		pages.setCustomValidity("Your book must have pages!");
+	} else pages.setCustomValidity("");
 }
-)
 
 function createBookFromForm() {
-    const readOrNot = function() {
-        if (bookForm.read.value==="Yes") {
-            return "read"
-        }
-        else if (bookForm.read.value==="No") {
-            return "not read"
-        }
-    }
-    // title, author, pages, read, note
-    addBookToLibrary(bookForm.title.value, bookForm.author.value, bookForm.pages.value, readOrNot(), bookForm.note.value)
-    updateCards()
-    bookForm.reset();
+	const readOrNot = function () {
+		if (bookForm.read.value === "Yes") {
+			return "read";
+		} else if (bookForm.read.value === "No") {
+			return "not read";
+		}
+	};
+	// title, author, pages, read, note
+	addBookToLibrary(
+		bookForm.title.value,
+		bookForm.author.value,
+		bookForm.pages.value,
+		readOrNot(),
+		bookForm.note.value,
+	);
+	updateCards();
+	bookForm.reset();
 }
 
 function addDemoBooks() {
-addBookToLibrary("Everyman", "Philip Roth", "182", "read", "Devastating. Recommend highly.");
-addBookToLibrary("Eloquent Javascript", "Marjin Haverbeke", "456", "not read", "Currently reading!");
-addBookToLibrary("Finnegans Wake", "James Joyce", "638", "not read", "Confusing and experimental narrative. Reconsidering.");
-addBookToLibrary("To The Lighthouse", "Virginia Woolf", "209", "read", "");
-addBookToLibrary("Il Canzoniere", "Francesco Petrarch", "800", "read", "Reminder: review analyses for poems on marked pages.");
+	addBookToLibrary(
+		"Everyman",
+		"Philip Roth",
+		"182",
+		"read",
+		"Devastating. Recommend highly.",
+	);
+	addBookToLibrary(
+		"Eloquent Javascript",
+		"Marjin Haverbeke",
+		"456",
+		"not read",
+		"Currently reading!",
+	);
+	addBookToLibrary(
+		"Finnegans Wake",
+		"James Joyce",
+		"638",
+		"not read",
+		"Confusing and experimental narrative. Reconsidering.",
+	);
+	addBookToLibrary("To The Lighthouse", "Virginia Woolf", "209", "read", "");
+	addBookToLibrary(
+		"Il Canzoniere",
+		"Francesco Petrarch",
+		"800",
+		"read",
+		"Reminder: review analyses for poems on marked pages.",
+	);
 }
 
-addDemoBooks()
-updateCards()
+addDemoBooks();
+updateCards();
